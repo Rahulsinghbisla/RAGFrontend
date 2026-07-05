a09a795b5b513997f88e4ef39d128d8ba64d66bf
RAHUL SINGH <rahulsingh@RAHULs-MacBook-Air.local>
2026-07-05 12:48:23 +0530

    Adding the .env

--- store.py
+++ store.py
@@ -10,6 +10,6 @@ pc = Pinecone(api_key="pcsk_4mGPBD_MSvWEvMCg5uJe1p2nbLY8VqiSRwuLbKZwdRWwP54cyvBt
 
 index = pc.Index("cuhbot")
 
-embeddings=OpenAIEmbeddings(api_key = "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")
+embeddings=OpenAIEmbeddings()
 
 vector_store = PineconeVectorStore(embedding=embeddings, index=index)
