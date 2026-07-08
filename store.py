7ed538e3426995210a00774a6a135076409c90c9
RAHUL SINGH <rahulsingh@RAHULs-MacBook-Air.local>
2026-07-08 12:36:26 +0530

    Changing the api

--- store.py
+++ store.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-pc = Pinecone(api_key="pcsk_6yfVHZ_JxYdjn9grNyhzpe4zray1FqChd5h4NGD833eX4BArSQymNS3MdLjcCNrL3RxFLR")
+pc = Pinecone(api_key="pcsk_4mGPBD_MSvWEvMCg5uJe1p2nbLY8VqiSRwuLbKZwdRWwP54cyvBtmKXXARYhH4JH4u1mqp")
 
 
 index = pc.Index("rag")
 
-embeddings=OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings=OpenAIEmbeddings()
 
 vector_store = PineconeVectorStore(embedding=embeddings, index=index)
